44779e8c999aab6facaabe93ecb65455897f5e50
steinko <steinkor@gmail.com>
2022-08-24 00:00:25 +0200

    first commit

---
+++ src/main/java/org/steinko/jpainstro/JpainstroApplication.java
@@ -0,0 +1,13 @@
+package org.steinko.jpainstro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpainstroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpainstroApplication.class, args);
+	}
+
+}
