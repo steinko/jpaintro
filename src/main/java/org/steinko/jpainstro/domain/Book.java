ef9b379fa5b4e98143c983fc7cdab5c558e1e1b7
steinko <steinkor@gmail.com>
2022-08-24 00:48:21 +0200

    crate class Book

--- src/main/java/org/steinko/jpainstro/domain/Book.java
+++ src/main/java/org/steinko/jpainstro/domain/Book.java
@@ -1,5 +1,14 @@
 package org.steinko.jpainstro.domain;
 
 public class Book {
+	private String publisher;
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
 
 }
--- src/test/java/org/steinko/jpainstro/domain/BookTest.java
+++ src/test/java/org/steinko/jpainstro/domain/BookTest.java
@@ -2,6 +2,7 @@ package org.steinko.jpainstro.domain;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BookTest {
 	
@@ -9,6 +10,15 @@ public class BookTest {
 	public void exists() {
 		Book book = new Book();
 				assertNotNull(book);
+		
+	}
+	
+	@Test
+	public void existAPubliser() {
+		Book book = new Book();
+		String publisher = "Cool Gay";
+		book.setPublisher(publisher);
+		assertEquals(book.getPublisher(), publisher);
 		
 	}
 
