4673330cc619b40371079a6d4cd2f596eb0e1baa
steinko <steinkor@gmail.com>
2022-08-26 13:43:18 +0200

    automate deployment of database

--- deployment/index.ts
+++ deployment/index.ts
@@ -1,9 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import {dataBase} from "./DataBase"
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
+dataBase
+export const endpoint = dataBase.endpoint
